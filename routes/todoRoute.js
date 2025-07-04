03634bd9fcee8fe6a4d7acd85d9efa395b33775d
Sandeep Kumar <sandeep.kumar@magicsw.com>
2025-07-04 11:21:09 +0530

    Adding new files

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -35,6 +35,19 @@ router.post("/", authMiddleware, async (req, res) => {
   }
 });
 
+router.get("/:id", authMiddleware, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await Todo.findOne({ _id: id, user: req.user._id });
+    if (!todo) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+    res.json(todo);
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
 router.put("/:id", authMiddleware, async (req, res) => {
   try {
     const { id } = req.params;
